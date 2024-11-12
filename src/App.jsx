1aed3b0eb54ae065065d7c35a31630bd0402e005
Lucy <kosmos.vera@gmail.com>
2024-11-12 18:47:53 +0100

    Lesson2

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Button from './components/Button/Button';
+import LessonCard from './components/LessonCard/LessonCard';
+
+function App() {
+  return (
+    <div className="App">
+ <LessonCard/>
+ <Button/>
+ <Button/>
+ </div>
+  );
+}
+
+export default App;
